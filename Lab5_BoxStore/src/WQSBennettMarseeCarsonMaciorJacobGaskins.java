fe565606c4f101fb0922c4283ce9a0e49868d588
J Gaskins <chainmelted@gmail.com>
2025-10-14 13:01:57 -0400

    seperated init inventory to assign & then add

--- Lab5_BoxStore/src/WQSBennettMarseeCarsonMaciorJacobGaskins.java
+++ Lab5_BoxStore/src/WQSBennettMarseeCarsonMaciorJacobGaskins.java
@@ -4,8 +4,13 @@ public class WQSBennettMarseeCarsonMaciorJacobGaskins {
     public static void main(String[] args) {
 
         ArrayList<StoreItem> storeInventory = new ArrayList<>(); // init the inventory of the store
-        storeInventory.add(new Fruit("Mango","Greenwise",4.00,5,"a mango","N//a",true,"12/31/1999"
-        ,"yellow/orange",true, 5));
+
+        Fruit mango = new Fruit("Mango","Greenwise",4.00,5,"a mango","N/a",true,"12/31/1999"
+                ,"yellow/orange",true, 5);
+        Vegetable carrot = new Vegetable("Carrot","Bugs", 5, 5, "an avg carrot", "N/a",
+        true, "12/31/1999","orange", true, 2,true);
+        storeInventory.add(mango);
+        storeInventory.add(carrot);
 
         System.out.println("Store's inventory:"); // going through each item in the store's inventory.
         for (StoreItem item : storeInventory) {
