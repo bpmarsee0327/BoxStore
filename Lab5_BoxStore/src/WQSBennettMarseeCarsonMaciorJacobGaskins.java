a69716ef7fe2a531534cc1e86008a9136f335ca8
J Gaskins <chainmelted@gmail.com>
2025-10-15 10:12:22 -0400

    made displayInventory method

--- Lab5_BoxStore/src/WQSBennettMarseeCarsonMaciorJacobGaskins.java
+++ Lab5_BoxStore/src/WQSBennettMarseeCarsonMaciorJacobGaskins.java
@@ -13,156 +13,162 @@ public class WQSBennettMarseeCarsonMaciorJacobGaskins {
         storeInventory.add(mango);
         storeInventory.add(carrot);
 
-        System.out.println("Store's inventory:"); // going through each item in the store's inventory.
-        for (StoreItem item : storeInventory) {
-            System.out.println(item);
-        }
+        displayInventory(storeInventory, "Store's init inventory");
         displayMenu(storeInventory);
 
         //Updated Inventory after adding/selling
-        System.out.println("Updated Inventory");
-        for (StoreItem item : storeInventory) {
-            System.out.println(item);}
+        displayInventory(storeInventory, "Store's final inventory");
     }
 
-        /**
-         * Display menu that allows the user to either add to the inventory or sell from the inventory
-         */
-        public static void displayMenu(ArrayList<StoreItem> storeInventory){
-            Scanner input = new Scanner(System.in);
-
-            while (true){
-                System.out.println("\nWould you like to sell an item or one to the inventory?");
-                System.out.println("1. Add item to inventory");
-                System.out.println("2. Sell item");
-                System.out.println("3. Exit");
-
-                int choice = input.nextInt();
-                input.nextLine();
-
-                switch (choice){
-                    case 1:
-                        addInventory(storeInventory);
-                        break;
-                    case 2:
-                        sellItem();
-                        break;
-                    case 3:
-                        System.out.println("Exiting");
-                        input.close();
-                        return;
-                    default:
-                        System.out.println("Invalid input.");
-                        break;
-                }
-            }
-        }
-        /**
-         * Menu that asks the user if they want to add an item to clothing, electronics, food, or household
-         */
-        public static void addInventory(ArrayList<StoreItem> storeInventory){
-            Scanner input = new Scanner(System.in);
-
-            while (true){
-                System.out.println("\nChoose a department to add items to: ");
-                System.out.println("1. Clothing");
-                System.out.println("2. Electronics");
-                System.out.println("3. Food");
-                System.out.println("4. Household");
-                System.out.println("5. Return to menu");
-
-                int choice = input.nextInt();
-                input.nextLine();
-
-                switch (choice){
-                    case 1:
-                        itemsinCategory(storeInventory, ClothingItem.class);
-                        addtoInventory(storeInventory, ClothingItem.class);
-
-                        break;
-                    case 2:
-                        itemsinCategory(storeInventory, ElectronicsItem.class);
-                        addtoInventory(storeInventory, ElectronicsItem.class);
-
-                        break;
-                    case 3:
-                        itemsinCategory(storeInventory, FoodItem.class);
-                        addtoInventory(storeInventory, FoodItem.class);
-
-                        break;
-                    case 4:
-                        itemsinCategory(storeInventory, HouseholdItem.class);
-                        addtoInventory(storeInventory, HouseholdItem.class);
-
-                        break;
-                    case 5:
-                        System.out.println("Returning");
-
-                        return;
-
-                    default:
-                        System.out.println("Invalid input.");
-                }
-            }
-        }
-        public static void sellItem(){
-            Scanner input = new Scanner(System.in);
-
-        }
-
     /**
-     * helper for addInventory to display items within selected category.
-     * @param storeInventory an array of the store's inventory
-     * @param type the class type. the category of item being looked for.
+     * Display menu that allows the user to either add to the inventory or sell from the inventory
      */
-    public static void itemsinCategory(ArrayList<StoreItem> storeInventory, Class type){
-            for (StoreItem item : storeInventory){
-                if (type.isInstance(item)) {
-                    System.out.println(item);
-                }
-            }
-        }
+    public static void displayMenu(ArrayList<StoreItem> storeInventory) {
+        Scanner input = new Scanner(System.in);
 
-    /**
-     * To add to preexisting items in the store's inventory.
-     * @param storeInventory the store's inventory.
-     * @param type the category.
-     */
-    public static void addtoInventory(ArrayList<StoreItem> storeInventory, Class type){
-            Scanner input = new Scanner(System.in);
-            System.out.println("Select if you would like to add an additional item already present or new item.");
-            System.out.println("1. Add more of an existing item.");
-            System.out.println("2. Create a new item.");
+        while (true) {
+            System.out.println("\nWould you like to sell an item or one to the inventory?");
+            System.out.println("1. Add item to inventory");
+            System.out.println("2. Sell item");
+            System.out.println("3. Exit");
 
             int choice = input.nextInt();
             input.nextLine();
 
-                if (choice == 1) {
-                    System.out.println("Enter the name of the item that you'd like more of. (Case-Sensitive)");
-                    String name = input.nextLine();
-                    boolean found = false;
+            switch (choice) {
+                case 1:
+                    addInventory(storeInventory);
+                    break;
+                case 2:
+                    sellItem();
+                    break;
+                case 3:
+                    System.out.println("Exiting");
+                    input.close();
+                    return;
+                default:
+                    System.out.println("Invalid input.");
+                    break;
+            }
+        }
+    }
 
-                    for (StoreItem item : storeInventory){
-                        if (item.getItemName().equals(name)){
-                            found = true; // to prevent accidentally notifying as not present when iterating through the rest of the list
-                            System.out.println("Enter how many more of the item you'd like.");
+    /**
+     * Menu that asks the user if they want to add an item to clothing, electronics, food, or household
+     */
+    public static void addInventory(ArrayList<StoreItem> storeInventory) {
+        Scanner input = new Scanner(System.in);
 
-                            //getting exists quantity through the accessor, adding to amt requested, then reassigning the attribute through mutator.
-                            int moreQuantity = input.nextInt();
-                            int newQuantity = moreQuantity + item.getQuantity();
-                            item.setQuantity(newQuantity);
-                        }
-                        }
-                    if (found == false){
-                        System.out.println("not present");
-                    }
-                    }
+        while (true) {
+            System.out.println("\nChoose a department to add items to: ");
+            System.out.println("1. Clothing");
+            System.out.println("2. Electronics");
+            System.out.println("3. Food");
+            System.out.println("4. Household");
+            System.out.println("5. Return to menu");
+
+            int choice = input.nextInt();
+            input.nextLine();
+
+            switch (choice) {
+                case 1:
+                    itemsinCategory(storeInventory, ClothingItem.class);
+                    addtoInventory(storeInventory, ClothingItem.class);
+
+                    break;
+                case 2:
+                    itemsinCategory(storeInventory, ElectronicsItem.class);
+                    addtoInventory(storeInventory, ElectronicsItem.class);
+
+                    break;
+                case 3:
+                    itemsinCategory(storeInventory, FoodItem.class);
+                    addtoInventory(storeInventory, FoodItem.class);
+
+                    break;
+                case 4:
+                    itemsinCategory(storeInventory, HouseholdItem.class);
+                    addtoInventory(storeInventory, HouseholdItem.class);
+
+                    break;
+                case 5:
+                    System.out.println("Returning");
+
+                    return;
+
+                default:
+                    System.out.println("Invalid input.");
+            }
+        }
+    }
+
+    public static void sellItem() {
+        Scanner input = new Scanner(System.in);
+
+    }
+
+    /**
+     * helper for addInventory to display items within selected category.
+     *
+     * @param storeInventory an array of the store's inventory
+     * @param type           the class type. the category of item being looked for.
+     */
+    public static void itemsinCategory(ArrayList<StoreItem> storeInventory, Class type) {
+        for (StoreItem item : storeInventory) {
+            if (type.isInstance(item)) {
+                System.out.println(item);
+            }
+        }
+    }
+
+    /**
+     * To add to preexisting items in the store's inventory.
+     *
+     * @param storeInventory the store's inventory.
+     * @param type           the category.
+     */
+    public static void addtoInventory(ArrayList<StoreItem> storeInventory, Class type) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Select if you would like to add an additional item already present or new item.");
+        System.out.println("1. Add more of an existing item.");
+        System.out.println("2. Create a new item.");
+
+        int choice = input.nextInt();
+        input.nextLine();
+
+        if (choice == 1) {
+            System.out.println("Enter the name of the item that you'd like more of. (Case-Sensitive)");
+            String name = input.nextLine();
+            boolean found = false;
+
+            for (StoreItem item : storeInventory) {
+                if (item.getItemName().equals(name)) {
+                    found = true; // to prevent accidentally notifying as not present when iterating through the rest of the list
+                    System.out.println("Enter how many more of the item you'd like.");
+
+                    //getting exists quantity through the accessor, adding to amt requested, then reassigning the attribute through mutator.
+                    int moreQuantity = input.nextInt();
+                    int newQuantity = moreQuantity + item.getQuantity();
+                    item.setQuantity(newQuantity);
+                }
+                displayInventory(storeInventory,"Updated store inventory");
+            }
+            if (found == false) {
+                System.out.println("not present");
+            }
+        }
 //                highlight below + hit Ctrl + /
 //                if (choice == 2 ){
 //                System.out.println("Enter the name of item you would like to add.");
 //                }
+    }
 
-                }
-                }
+    public static void displayInventory(ArrayList<StoreItem> storeInventory, String message) {
+        System.out.println(message);
+        for (StoreItem item : storeInventory) {
+            System.out.println(item);
+        }
+    }
+}
 
 
